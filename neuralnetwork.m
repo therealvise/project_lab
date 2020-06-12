011b46eef911fbaf328b008d65afb8ad9c599d9e
therealvise <52854722+therealvise@users.noreply.github.com>
2020-06-12 18:54:45 +0200

    Update neuralnetwork.m

--- neuralnetwork.m
+++ neuralnetwork.m
@@ -1,7 +1,16 @@
+% Script con lo scopo di addestrare una rete neurale sulla base di un
+% dataset contenente circa 1300 immagini, divise in base alla classe di
+% appartenenza. Il dataset è disponibile presso il link:
+% https://www.kaggle.com/slothkong/10-monkey-species
+
+% La scelta è ricaduta su una rete AlexNet, di tipo convoluzionale,
+% ampiamente utilizzata nel riconoscimento delle immagini
 net = alexnet;
 analyzeNetwork(net)
 inputSize = net.Layers(1).InputSize;
 
+% Import immagini da locale. In caso di esecuzione su una macchina diversa,
+% cambiare il percorso
 folder_train = '/Users/gianlucavisentin/Desktop/monkeys_project/data_train';
 myImages_train = imageSet(folder_train);
 
@@ -10,7 +19,8 @@ myImages_val = imageSet(folder_validation);
 
 imdsTrain = imageDatastore(folder_train, 'LabelSource', 'foldernames', 'IncludeSubfolders',true);
 imdsValidation = imageDatastore(folder_validation, 'LabelSource', 'foldernames', 'IncludeSubfolders',true);
-%imdsValidation = imageDatastore('validation', 'IncludeSubfolders', true, 'LabelSource', 'foldernames');
+
+% Training:Test split 80-20
 [trainingImages, testImages] = splitEachLabel(imdsTrain, 0.8, 'randomize');
 layers = net.Layers;
 
@@ -18,7 +28,7 @@ layersTransfer = net.Layers(1:end-3);
 
 
 numClasses = 10;
-
+% Definizione dei layers convoluzionali 
 layers = [
     layersTransfer
     fullyConnectedLayer(numClasses,'WeightLearnRateFactor',20,'BiasLearnRateFactor',20)
@@ -26,6 +36,10 @@ layers = [
     classificationLayer];
 
 pixelRange = [-30 30];
+
+% Data augmentation viene introdotto per migliorare i risultati ed evitare
+% il fenomeno dell'overfitting, ovvero quando un modello statistico
+% complesso si adatta ai dati di campione
 imageAugmenter = imageDataAugmenter( ...
     'RandXReflection',true, ...
     'RandXTranslation',pixelRange, ...
@@ -35,6 +49,8 @@ augimdsTrain = augmentedImageDatastore(inputSize(1:2),trainingImages, ...
 
 augimdsValidation = augmentedImageDatastore(inputSize(1:2),testImages);
 
+% Nel caso di un'esecuzione più veloce si può diminuire il numero di
+% epoche, riducendo le iterazionii
 options = trainingOptions('sgdm', ...
     'MiniBatchSize',10, ...
     'MaxEpochs',6, ...
@@ -52,7 +68,7 @@ netTransfer = trainNetwork(augimdsTrain,layers,options);
 YValidation = testImages.Labels;
 accuracy = mean(YPred == YValidation)
 
-
+% Print di un esempio per ogni immagine, appartenente ad una classe
 idx = randperm(numel(testImages.Files),12);
 figure
 for i = 1:12
