42dbf2330786b3c13b474bf79e9362cf57f8ffab
lucadisimone <66620587+lucadisimone@users.noreply.github.com>
2020-06-15 17:31:12 +0200

    Update imageProcessing.m

--- imageProcessing.m
+++ imageProcessing.m
@@ -3,11 +3,11 @@
 %Le immagini del Dataset di partenza sono modificate applicando rumore Gaussiano e filtro di Wiener.
 %In entrambi i casi le immagini sono state salvate in due directory distinte per poter essere analizzate separatamente.
 
-%percorso della cartella dove sono presenti le immagini della i-esima specie di scimmia del Dataset 
+%Percorso della cartella dove sono presenti le immagini della i-esima specie di scimmia del Dataset 
 path_directory='/Users/nomeUtente/Desktop/10449_44567_bundle_archive/training/training/n0'; 
 original_files=dir([path_directory '/*.jpg']);
 
-%ciclo per scorrere tutte le immagini della cartella per k che da 1 all'ultimo elemento della cartella
+%Ciclo per scorrere tutte le immagini della cartella per k che da 1 all'ultimo elemento della cartella
 %Ad ogni immagine è stato applicato nell'ordine: conversione da RGB a scala di grigi, rumore Gaussiano e filtro di Wiener.
 for k=1:length(original_files)
     filename=[path_directory '/' original_files(k).name];
