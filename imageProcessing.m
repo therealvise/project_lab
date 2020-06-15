61b04a03586fc0066d5daa0ca3c5dd8d7e5bf809
lucadisimone <66620587+lucadisimone@users.noreply.github.com>
2020-06-15 18:06:35 +0200

    Update imageProcessing.m

--- imageProcessing.m
+++ imageProcessing.m
@@ -1,15 +1,15 @@
 
-%Questo script è relativo alla modifica del Dataset.
-%Le immagini del Dataset di partenza sono modificate applicando rumore Gaussiano e filtro di Wiener.
-%In entrambi i casi le immagini sono state salvate in due directory distinte per poter essere analizzate separatamente.
+% Questo script è relativo alla modifica del Dataset.
+% Le immagini del Dataset di partenza sono modificate applicando rumore Gaussiano e filtro di Wiener.
+% In entrambi i casi le immagini sono state salvate in due directory distinte per poter essere analizzate separatamente.
 
-%Percorso della cartella dove sono presenti le immagini della i-esima specie di scimmia del Dataset
-%In caso di esecuzione su una macchina diversa, cambiare il percorso
+% Percorso della cartella dove sono presenti le immagini della i-esima specie di scimmia del Dataset
+% In caso di esecuzione su una macchina diversa, cambiare il percorso
 path_directory='/Users/nomeUtente/Desktop/10449_44567_bundle_archive/training/training/n0'; 
 original_files=dir([path_directory '/*.jpg']);
 
-%Ciclo per scorrere tutte le immagini della cartella per k che da 1 all'ultimo elemento della cartella
-%Ad ogni immagine è stato applicato nell'ordine: conversione da RGB a scala di grigi, rumore Gaussiano e filtro di Wiener.
+% Ciclo per scorrere tutte le immagini della cartella per k che da 1 all'ultimo elemento della cartella
+% Ad ogni immagine è stato applicato nell'ordine: conversione da RGB a scala di grigi, rumore Gaussiano e filtro di Wiener.
 for k=1:length(original_files)
     filename=[path_directory '/' original_files(k).name];
     R = imread(filename)
