75ac1aecfd70b895db62a06e71107b0de7e0a288
giulioraponi <66562295+giulioraponi@users.noreply.github.com>
2020-06-18 17:00:14 +0200

    Update histogram_bivariateGB.m

--- histogram_bivariateGB.m
+++ histogram_bivariateGB.m
@@ -1,11 +1,16 @@
-rgb = imread ('/Users/nomeutente/Desktop/monkeys_project/data_train/n0/n0021.jpg');
+% In questo script andiamo ad effettuare un'analisi sui colori.
+% Nello specifico elaboriamo l'immagine a colori "n0021.png" raffigurante un esempio di scimmia appartenente
+% alla specie 'Alouatta Palliata' tracciando un istogramma bivariato dei valori RGB Verde e Blu per ogni pixel,
+% per visualizzare la distribuzione del colore.
+
+rgb = imread ('/Users/nomeUtente/Desktop/monkeys_project/training/n0/n0021.jpg');
 imshow(rgb)
 histogram2(g,b,'DisplayStyle','tile','ShowEmptyBins','on',...
     'XBinLimits',[0 255],'YBinLimits',[0 255]);
 axis equal
 colorbar
-xlabel('Green Values')
-ylabel('Blue Values')
-title('Green vs. Blue Pixel Components')
+xlabel('Valori Verde')
+ylabel('Valori Blu')
+title('Componenti a pixel Verde vs. Blu')
 ax = gca;
 ax.CLim = [0 500];
